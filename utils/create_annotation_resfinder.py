9527736f4c213549bf3844baf766cb13990dcfe2
mtva0001 <51121156+mtva0001@users.noreply.github.com>
2024-10-10 17:08:12 +0200

    Create create_annotation_resfinder.py

---
+++ utils/create_annotation_resfinder.py
@@ -0,0 +1,29 @@
+#This script creates an annotation csv file that can be used to link the ARG IDs (reported in the blastp output files) to ARG classes, sources and antibiotics.
+
+import os
+import pandas as pd
+
+# Path to the ResFinderFG2.0 AA file
+faa_file_path = 'ResFinder_FG_AA.faa'
+# Path to the output CSV file
+output_csv_path = 'annotation_resfinder.csv'
+
+# Initialize a list to store the header information
+headers_data = []
+
+# Read the .faa file
+with open(faa_file_path, 'r') as file:
+    for line in file:
+        # Check if the line starts with '>'
+        if line.startswith('>'):
+            # Remove the '>' and split by '|'
+            header = line[1:].strip().split('|')
+            headers_data.append(header)
+
+# Create a DataFrame from the headers data
+headers_df = pd.DataFrame(headers_data, columns=['ARG', 'ID', 'Source', 'AB'])
+
+# Save the DataFrame to a CSV file
+headers_df.to_csv(output_csv_path, index=False)
+
+print(f"Annotation CSV file saved to {output_csv_path}")
