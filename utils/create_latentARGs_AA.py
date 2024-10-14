17ed35cae845790c848c961a8135c1f70e82b814
Máté Vass <51121156+mtva0001@users.noreply.github.com>
2024-10-14 15:44:29 +0200

    Create create_latentARGs_AA.py

---
+++ utils/create_latentARGs_AA.py
@@ -0,0 +1,23 @@
+# Table S5 from the paper https://doi.org/10.1186/s40168‑023‑01479‑0 was used to create a AA fasta file for Latent ARGs.
+
+import csv
+from Bio.Seq import Seq
+
+# Function to translate DNA sequences to protein sequences and write to FASTA
+def translate_dna_to_protein(input_csv, output_fasta):
+    with open(input_csv, 'r') as csvfile, open(output_fasta, 'w') as fastafile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        
+        for row in reader:
+            header = row[0]  # Header from the 1st column
+            dna_sequence = row[3]  # DNA sequence from the 4th column
+            
+            # Translate DNA to protein and remove stop codons (*)
+            protein_sequence = str(Seq(dna_sequence).translate()).replace('*', '')
+            
+            fastafile.write(f">{header}\n")
+            fastafile.write(f"{protein_sequence}\n")
+
+
+translate_dna_to_protein('Latent_ARGs.csv', 'Latent_ARGs_AA.fasta')
