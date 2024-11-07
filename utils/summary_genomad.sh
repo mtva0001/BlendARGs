29b4b35e945e45603ddfe5dd78b3183d8c7354de
Máté Vass <51121156+mtva0001@users.noreply.github.com>
2024-11-07 12:14:14 +0100

    Create summary_genomad.sh

---
+++ utils/summary_genomad.sh
@@ -0,0 +1,32 @@
+#This script create two summary files for plasmids and viruses identified by geNomad
+#!/bin/bash
+
+# Set paths for your geNomad directory and output summary files
+genomad_dir="result_BlendARGs/VirusIdentification/geNomad"
+virus_summary="virus_summary_Marine.tsv"
+plasmid_summary="plasmid_summary_Marine.tsv"
+
+# Clear or create the summary files with headers
+echo -e "Sample\t$(head -n 1 $(find $genomad_dir -type f -name "*_virus_summary.tsv" | head -n 1))" > $virus_summary
+echo -e "Sample\t$(head -n 1 $(find $genomad_dir -type f -name "*_plasmid_summary.tsv" | head -n 1))" > $plasmid_summary
+
+# Loop through each sample folder and process the .tsv files
+for sample_folder in "$genomad_dir"/S*; do
+    sample_name=$(basename "$sample_folder")
+
+    # Paths to the virus and plasmid summary files
+    virus_file="$sample_folder/MEGAHIT-${sample_name}.contigs_summary/MEGAHIT-${sample_name}.contigs_virus_summary.tsv"
+    plasmid_file="$sample_folder/MEGAHIT-${sample_name}.contigs_summary/MEGAHIT-${sample_name}.contigs_plasmid_summary.tsv"
+
+    # Append data to virus summary file if it exists
+    if [ -f "$virus_file" ]; then
+        tail -n +2 "$virus_file" | awk -v sample="$sample_name" '{print sample"\t"$0}' >> $virus_summary
+    fi
+
+    # Append data to plasmid summary file if it exists
+    if [ -f "$plasmid_file" ]; then
+        tail -n +2 "$plasmid_file" | awk -v sample="$sample_name" '{print sample"\t"$0}' >> $plasmid_summary
+    fi
+done
+
+echo "Summaries compiled into $virus_summary and $plasmid_summary."
